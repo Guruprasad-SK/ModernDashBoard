6b2e8aae22889c3b40c42a4f884b92ea048bba79
guruf <guru@VS2017EE-PC>
2024-02-05 18:29:52 +0530

    Add project files.

---
+++ ModernDashBoard/ViewModel/NavigationViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Data;
+using System.Windows.Input;
+using ModernDashBoard.Model;
+using static ModernDashBoard.Model.MainModel;
+
+
+
+
+namespace ModernDashBoard.ViewModel
+{
+    class NavigationViewModel : INotifyPropertyChanged
+    {
+        private CollectionViewSource MenuItemsCollection;
+        public ICollectionView SourceCollection => MenuItemsCollection.View;
+        public NavigationViewModel()
+        {
+            // ObservableCollection represents a dynamic data collection that provides notifications when items
+            // get added, removed, or when the whole list is refreshed.
+            ObservableCollection<MenuItems> menuItems = new ObservableCollection<MenuItems>
+            {
+                new MenuItems { MenuName = "Home", MenuImage = @"Assets/Home_Icon.png" },
+                new MenuItems { MenuName = "Desktop", MenuImage = @"Assets/Desktop_Icon.png" },
+                new MenuItems { MenuName = "Documents", MenuImage = @"Assets/Document_Icon.png" },
+                new MenuItems { MenuName = "Downloads", MenuImage = @"Assets/Download_Icon.png" },
+                new MenuItems { MenuName = "Pictures", MenuImage = @"Assets/Images_Icon.png" },
+                new MenuItems { MenuName = "Music", MenuImage = @"Assets/Music_Icon.png" },
+                new MenuItems { MenuName = "Movies", MenuImage = @"Assets/Movies_Icon.png" },
+                new MenuItems { MenuName = "Trash", MenuImage = @"Assets/Trash_Icon.png" }
+
+
+            };
+            MenuItemsCollection = new CollectionViewSource { Source = menuItems };
+            
+            MenuItemsCollection.Filter += MenuItems_Filter;
+
+            // Set Startup Page
+           
+
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        private string filterText;
+        public string FilterText
+        {
+            get => filterText;
+            set
+            {
+                filterText = value;
+                MenuItemsCollection.View.Refresh();
+                OnPropertyChanged("FilterText");
+            }
+        }
+
+        private void MenuItems_Filter(object sender, FilterEventArgs e)
+        {
+            if (string.IsNullOrEmpty(FilterText))
+            {
+                e.Accepted = true;
+                return;
+            }
+
+            MenuItems _item = e.Item as MenuItems;
+            if (_item.MenuName.ToUpper().Contains(FilterText.ToUpper()))
+            {
+                e.Accepted = true;
+            }
+            else
+            {
+                e.Accepted = false;
+            }
+        }
+        private object _selectedViewModel;
+        public object SelectedViewModel
+        {
+            get => _selectedViewModel;
+            set { _selectedViewModel = value; OnPropertyChanged("SelectedViewModel"); }
+        }
+      
+
+
+    }
+}
+
+
